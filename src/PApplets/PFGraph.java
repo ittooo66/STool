15702fc5434f52068c95eb5f11b116dff007f8f6
ito <ito.s.bd@m.titech.ac.jp>
2015-10-11 21:41:36 +0900

    Processing各種ウィンドウ追加

---
+++ src/PApplets/PFGraph.java
@@ -0,0 +1,45 @@
+package PApplets;
+
+import processing.core.PApplet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 66 on 2015/10/11.
+ */
+public class PFGraph extends PApplet {
+	List<Integer> mouseX_temp=new ArrayList();
+	List<Integer> mouseY_temp=new ArrayList();
+
+
+	public void draw(){
+
+		background(0);
+		stroke(200,200,200);
+
+		text("W:"+width+",H:"+height+" on PF",mouseX,mouseY);
+
+		for(int x: mouseX_temp){
+			line(x,10,x,height-10);
+		}
+
+		for(int y: mouseY_temp){
+			line(10,y,width-10,y);
+		}
+
+	}
+
+	public  void setup(){
+		size(1024,768);
+		noLoop();
+	}
+
+	public void mousePressed(){
+		mouseX_temp.add(mouseX);
+		mouseY_temp.add(mouseY);
+
+		redraw();
+	}
+
+}
---
+++ src/PApplets/UCGraph.java
@@ -0,0 +1,7 @@
+package PApplets;
+
+/**
+ * Created by 66 on 2015/10/11.
+ */
+public class UCGraph {
+}
