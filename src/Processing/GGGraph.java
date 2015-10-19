3cfcb8857e540e2290c25ce0d45d2de02b545f02
ito <ito.s.bd@m.titech.ac.jp>
2015-10-20 05:58:01 +0900

    GGProcessing周り、おわり

--- src/Processing/GGGraph.java
+++ src/Processing/GGGraph.java
@@ -40,23 +40,15 @@ public class GGGraph extends PApplet{
 		//背景描画
 		background(COLOR_BACKGROUND);
 
-		//デバッグ用
-		fill(COLOR_LINES);
-		textAlign(LEFT);
-		text("Version:"+sToolEditor.getVersion().toString()+", Mode:"+sToolEditor.getViewmode().toString()+", Resolution:"+width+","+height+"(for debugging)",10,20);
-
 		//枝周り記述の下準備
 		stroke(COLOR_LINES);
 		strokeWeight(1);
 		noFill();
 
-		//TODO:AND記述
 		//AND記述対象のゴール捜索
 		for(Goal parentGoal: sToolEditor.fgm.getGoals()) {
 			if (parentGoal.childrenType.equals(Goal.ChildrenType.AND)) {
 
-				//ゴール下のマージン
-				final int mergin = 40;
 				//子ゴールカウント
 				int countOfChildGoals = 0;
 				//開始角度・終了角度を用意
@@ -66,22 +58,14 @@ public class GGGraph extends PApplet{
 				for (Goal childGoal : sToolEditor.fgm.getGoals()) {
 					if (childGoal.parentId == parentGoal.id) {
 
-						//子ゴールの角度（0~2PIで４->３->２->１象限の順にまわる）
-						float childR = (PI/2) + atan((float) (childGoal.y - parentGoal.y) / (float) (childGoal.x - parentGoal.x));
-						//第２、３象限のとき、atan()の都合上修正噛ます
-						if (childGoal.x < parentGoal.x)childR += PI;
-						if (childR < PI/2){
-							childR += 3*PI/2;
-						}else{
-							childR -= PI/2;
-						}
-						System.out.println(childR);
+						//ゴール角度
+						float childR = getRadian(parentGoal, childGoal);
 
 						//1st Goalのとき、とりあえずtemp にいれちゃう
 						if (countOfChildGoals == 0) {
 							rootR=childR;
 							distR=childR;
-						} else if(rootR > childR){//それ以外
+						} else if(rootR > childR){
 							rootR=childR;
 						}else if(distR < childR){
 							distR=childR;
@@ -89,28 +73,43 @@ public class GGGraph extends PApplet{
 						countOfChildGoals++;
 					}
 
-
 					//ゴールカウントが２以上で線引き
 					if (countOfChildGoals > 1) {
-						text(rootR + "," + distR, 10, 100);
-						arc(parentGoal.x, parentGoal.y, textWidth(parentGoal.name) + 40 + 60, 40 + 50, rootR, distR);
+						//arcずれは円形arcで一旦対処
+						arc(parentGoal.x, parentGoal.y, textWidth(parentGoal.name) + 40 + 30,  40 + 30, rootR, distR);
 					}
 				}
 			}
 		}
 
-
 		//枝を描画
-		for (Goal g : sToolEditor.fgm.getGoals()) {
-			if (g.parentId != -1)
-				line(g.x, g.y, sToolEditor.fgm.getGoalById(g.parentId).x, sToolEditor.fgm.getGoalById(g.parentId).y);
+		fill(COLOR_LINES);
+		for (Goal childGoal : sToolEditor.fgm.getGoals()) {
+			if (childGoal.parentId != -1) {
+				//親ゴール取得
+				Goal parentGoal = sToolEditor.fgm.getGoalById(childGoal.parentId);
+
+				//ゴールの方角
+				float childR = getRadian(childGoal, parentGoal);
+				float parentR= getRadian(parentGoal, childGoal);
+
+				//x,y座標系
+				float xC=((textWidth(childGoal.name)+40)/2)*cos(childR);
+				float yC=(40/2)*sin(childR);
+				float xP=((textWidth(childGoal.name)+40)/2)*cos(parentR);
+				float yP=(40/2)*sin(parentR);
+
+				line(xP+parentGoal.x, yP+parentGoal.y, xC+childGoal.x, yC+childGoal.y);
+				ellipse(xC+childGoal.x,yC+childGoal.y,10,10);
+			}
 		}
 
 		//各ゴールを描画
-			textAlign(CENTER, CENTER);
+		textAlign(CENTER, CENTER);
 		for (Goal g : sToolEditor.fgm.getGoals()) {
-			//背景と同色で塗りつぶし（背面の枝塗りつぶしのため）
+
 			fill(COLOR_BACKGROUND);
+			stroke(COLOR_LINES);
 
 			//ゴールの優先度に応じた配色設定
 			if (g.id == selectedGoalId) {
@@ -124,15 +123,46 @@ public class GGGraph extends PApplet{
 				strokeWeight(1);
 			}
 
+			//葉っぱかどうかで変わる配色設定
+			if(g.childrenType.equals(Goal.ChildrenType.LEAF)) fill(COLOR_LINES);
+
 			//わっか記述
 			ellipse(g.x, g.y, textWidth(g.name) + 40, 40);
 
-			//名前の記述
 			fill(COLOR_LINES);
+
+			//葉っぱかどうかで変わる配色設定
+			if(g.childrenType.equals(Goal.ChildrenType.LEAF)) fill(COLOR_BACKGROUND);
+
+			//名前の記述
 			text(g.name, g.x, g.y - 2);
 		}
+
 	}
 
+	/**
+	 * ２つのゴールのなす角度を返す
+	 * @param root 基点ゴール
+	 * @param dist 終点ゴール
+	 * @return rootから見てdistがどこの方面（ラジアン）にあるかを返す。
+	 * 東：0 (2*PI)
+	 * 南：PI/2
+	 * 西：PI
+	 * 北：3*PI/2
+	 * となる
+	 */
+	private float getRadian(Goal root, Goal dist){
+		//枝の刺さる角度
+		float alpha = (PI/2) + atan((float) ( dist.y - root.y) / (float) (dist.x - root.x ));
+		//第２、３象限のとき、atan()の都合上修正噛ます
+		if (root.x > dist.x)alpha += PI;
+		if (alpha < PI/2){
+			alpha += 3*PI/2;
+		}else{
+			alpha -= PI/2;
+		}
+		return alpha;
+	}
 
 	public void mousePressed(){
 		final int goalMergin = 40;
