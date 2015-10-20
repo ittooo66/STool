10f735d2276944aa750832319ba89d794e8d0973
ito <ito.s.bd@m.titech.ac.jp>
2015-10-20 15:00:34 +0900

    フォーマッタかけた

--- src/Processing/GGGraph.java
+++ src/Processing/GGGraph.java
@@ -8,7 +8,7 @@ import processing.core.PFont;
 /**
  * ゴールグラフエディタタブで出力するProcessing部分
  */
-public class GGGraph extends PApplet{
+public class GGGraph extends PApplet {
 
 	//選択中のゴールID（-1なら非選択）
 	public int selectedGoalId = -1;
@@ -17,25 +17,25 @@ public class GGGraph extends PApplet{
 	private SToolEditor sToolEditor;
 
 	//カラーパレット
-	private final int COLOR_BACKGROUND =color(28,28,28);
-	private final int COLOR_LINES =color(123,144,210);
-	private final int COLOR_SELECTED = color(226,148,59);
+	private final int COLOR_BACKGROUND = color(28, 28, 28);
+	private final int COLOR_LINES = color(123, 144, 210);
+	private final int COLOR_SELECTED = color(226, 148, 59);
 
-	public GGGraph(SToolEditor sToolEditor){
-		this.sToolEditor=sToolEditor;
+	public GGGraph(SToolEditor sToolEditor) {
+		this.sToolEditor = sToolEditor;
 	}
 
-	public void setup(){
+	public void setup() {
 		//とりあえず適当な解像度で初期化
-		size(1024,768);
+		size(1024, 768);
 		//CPU節約
 		noLoop();
 		//Font設定。
-		PFont font = createFont("メイリオ ボールド",15,true);
+		PFont font = createFont("メイリオ ボールド", 15, true);
 		textFont(font);
 	}
 
-	public void draw(){
+	public void draw() {
 		//背景描画
 		background(COLOR_BACKGROUND);
 
@@ -45,13 +45,13 @@ public class GGGraph extends PApplet{
 		noFill();
 
 		//AND記述対象のゴール捜索
-		for(Goal parentGoal: sToolEditor.fgm.getGoals()) {
+		for (Goal parentGoal : sToolEditor.fgm.getGoals()) {
 			if (parentGoal.childrenType.equals(Goal.ChildrenType.AND)) {
 
 				//子ゴールカウント
 				int countOfChildGoals = 0;
 				//開始角度・終了角度を用意
-				float rootR = 0, distR = PI*2;
+				float rootR = 0, distR = PI * 2;
 
 				//AND記述対象の子ゴール捜索
 				for (Goal childGoal : sToolEditor.fgm.getGoals()) {
@@ -62,12 +62,12 @@ public class GGGraph extends PApplet{
 
 						//1st Goalのとき、とりあえずtemp にいれちゃう
 						if (countOfChildGoals == 0) {
-							rootR=childR;
-							distR=childR;
-						} else if(rootR > childR){
-							rootR=childR;
-						}else if(distR < childR){
-							distR=childR;
+							rootR = childR;
+							distR = childR;
+						} else if (rootR > childR) {
+							rootR = childR;
+						} else if (distR < childR) {
+							distR = childR;
 						}
 						countOfChildGoals++;
 					}
@@ -75,7 +75,7 @@ public class GGGraph extends PApplet{
 					//ゴールカウントが２以上で線引き
 					if (countOfChildGoals > 1) {
 						//arcずれは円形arcで一旦対処
-						arc(parentGoal.x, parentGoal.y, textWidth(parentGoal.name) + 40 + 30,  40 + 30, rootR, distR);
+						arc(parentGoal.x, parentGoal.y, textWidth(parentGoal.name) + 40 + 30, 40 + 30, rootR, distR);
 					}
 				}
 			}
@@ -90,16 +90,16 @@ public class GGGraph extends PApplet{
 
 				//ゴールの方角
 				float childR = getRadian(childGoal, parentGoal);
-				float parentR= getRadian(parentGoal, childGoal);
+				float parentR = getRadian(parentGoal, childGoal);
 
 				//x,y座標系
-				float xC=((textWidth(childGoal.name)+40)/2)*cos(childR);
-				float yC=(40/2)*sin(childR);
-				float xP=((textWidth(parentGoal.name)+40)/2)*cos(parentR);
-				float yP=(40/2)*sin(parentR);
+				float xC = ((textWidth(childGoal.name) + 40) / 2) * cos(childR);
+				float yC = (40 / 2) * sin(childR);
+				float xP = ((textWidth(parentGoal.name) + 40) / 2) * cos(parentR);
+				float yP = (40 / 2) * sin(parentR);
 
-				line(xP+parentGoal.x, yP+parentGoal.y, xC+childGoal.x, yC+childGoal.y);
-				ellipse(xC+childGoal.x,yC+childGoal.y,10,10);
+				line(xP + parentGoal.x, yP + parentGoal.y, xC + childGoal.x, yC + childGoal.y);
+				ellipse(xC + childGoal.x, yC + childGoal.y, 10, 10);
 			}
 		}
 
@@ -117,7 +117,7 @@ public class GGGraph extends PApplet{
 			}
 
 			//葉っぱかどうかで変わる配色設定
-			if(g.childrenType.equals(Goal.ChildrenType.LEAF)) fill(COLOR_LINES);
+			if (g.childrenType.equals(Goal.ChildrenType.LEAF)) fill(COLOR_LINES);
 
 			//わっか記述
 			ellipse(g.x, g.y, textWidth(g.name) + 40, 40);
@@ -125,10 +125,10 @@ public class GGGraph extends PApplet{
 			fill(COLOR_LINES);
 
 			//葉っぱかどうかで変わる配色設定
-			if(g.childrenType.equals(Goal.ChildrenType.LEAF)) fill(COLOR_BACKGROUND);
+			if (g.childrenType.equals(Goal.ChildrenType.LEAF)) fill(COLOR_BACKGROUND);
 
 			//選択ゴール時
-			if(g.id ==selectedGoalId)fill(COLOR_SELECTED);
+			if (g.id == selectedGoalId) fill(COLOR_SELECTED);
 
 			//名前の記述
 			text(g.name, g.x, g.y - 2);
@@ -138,6 +138,7 @@ public class GGGraph extends PApplet{
 
 	/**
 	 * ２つのゴールのなす角度を返す
+	 *
 	 * @param root 基点ゴール
 	 * @param dist 終点ゴール
 	 * @return rootから見てdistがどこの方面（ラジアン）にあるかを返す。
@@ -147,20 +148,20 @@ public class GGGraph extends PApplet{
 	 * 北：3*PI/2
 	 * となる
 	 */
-	private float getRadian(Goal root, Goal dist){
+	private float getRadian(Goal root, Goal dist) {
 		//枝の刺さる角度
-		float alpha = (PI/2) + atan((float) ( dist.y - root.y) / (float) (dist.x - root.x ));
+		float alpha = (PI / 2) + atan((float) (dist.y - root.y) / (float) (dist.x - root.x));
 		//第２、３象限のとき、atan()の都合上修正噛ます
-		if (root.x > dist.x)alpha += PI;
-		if (alpha < PI/2){
-			alpha += 3*PI/2;
-		}else{
-			alpha -= PI/2;
+		if (root.x > dist.x) alpha += PI;
+		if (alpha < PI / 2) {
+			alpha += 3 * PI / 2;
+		} else {
+			alpha -= PI / 2;
 		}
 		return alpha;
 	}
 
-	public void mousePressed(){
+	public void mousePressed() {
 		final int goalMergin = 40;
 
 		//ドメイン選択の一時解除
@@ -176,10 +177,10 @@ public class GGGraph extends PApplet{
 		sToolEditor.redraw();
 	}
 
-	public void mouseDragged(){
+	public void mouseDragged() {
 		if (mouseButton == LEFT && selectedGoalId != -1) {
 			Goal g = sToolEditor.fgm.getGoalById(selectedGoalId);
-			if (g != null && 0<mouseX && mouseX<width && 0<mouseY && mouseY<height)
+			if (g != null && 0 < mouseX && mouseX < width && 0 < mouseY && mouseY < height)
 				sToolEditor.fgm.editGoal(selectedGoalId, mouseX, mouseY);
 		}
 		sToolEditor.redraw();
