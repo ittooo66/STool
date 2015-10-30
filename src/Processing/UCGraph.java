af4a7f59691888defb29e0df16c56afd84bc995c
ito <ito.s.bd@m.titech.ac.jp>
2015-10-31 04:05:58 +0900

    UCGRAPH雛形生成

--- src/Processing/UCGraph.java
+++ src/Processing/UCGraph.java
@@ -1,32 +1,65 @@
 package Processing;
 
+import Core.SToolEditor;
 import processing.core.PApplet;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by 66 on 2015/10/11.
  */
 public class UCGraph extends PApplet {
-	List<Integer> mouseX_temp = new ArrayList();
-	List<Integer> mouseY_temp = new ArrayList();
+	//選択中のUsecaseID（-1なら非選択）
+	public int selectedUsecaseId = -1;
+	public int selectedFlowId = -1;
+	public int selectedStepId = -1;
 
+	//本体
+	private SToolEditor sToolEditor;
+
+	//カラーパレット
+	private final int COLOR_BACKGROUND = color(28, 28, 28);
+	private final int COLOR_LINES = color(123, 144, 210);
+	private final int COLOR_SELECTED = color(226, 148, 59);
+
+	public UCGraph(SToolEditor sToolEditor) {
+		this.sToolEditor = sToolEditor;
+	}
 
 	public void draw() {
+		background(COLOR_BACKGROUND);
+		noFill();
+		stroke(COLOR_LINES);
+		strokeWeight(2);
+		text(width + "," + height, 10, 15);
 
-		background(0);
-		stroke(200, 200, 200);
+		//マージン
+		int m = 20;
+		//カラム幅
+		int cw = (width - 4 * m) / 3;
 
-		text("W:" + width + ",H:" + height + " on UC", mouseX, mouseY);
+		//Usecase枠線
+		rect(m, m, cw, height - 2 * m);
+		//Flow枠線:MAIN
+		rect(2 * m + cw, m, cw, m);
+		//Flow枠線:Exc,Alt
+		int alt_exc_height = (height - 5 * m) / 2;
+		rect(2 * m + cw, 3 * m, cw, alt_exc_height);
+		rect(2 * m + cw, 4 * m + alt_exc_height, cw, alt_exc_height);
+		//Step枠線
+		rect(3 * m + 2 * cw, m, cw, height - 2 * m);
 
-		for (int x : mouseX_temp) {
-			line(x, 10, x, height - 10);
-		}
-
-		for (int y : mouseY_temp) {
-			line(10, y, width - 10, y);
-		}
+		//追加・削除ボタン（仮置き）
+		//Usecase
+		ellipse(3 * m / 2, 3 * m / 2, m, m);
+		ellipse(5 * m / 2, 3 * m / 2, m, m);
+		//Flow:Alt
+		ellipse(5 * m / 2 + cw, 7 * m / 2, m, m);
+		ellipse(7 * m / 2 + cw, 7 * m / 2, m, m);
+		//Flow:Exc
+		ellipse(5 * m / 2 + cw, 9 * m / 2 + alt_exc_height, m, m);
+		ellipse(7 * m / 2 + cw, 9 * m / 2 + alt_exc_height, m, m);
+		//Step
+		ellipse(7 * m / 2 + 2 * cw, 3 * m / 2, m, m);
+		ellipse(9 * m / 2 + 2 * cw, 3 * m / 2, m, m);
 
 	}
 
@@ -36,8 +69,6 @@ public class UCGraph extends PApplet {
 	}
 
 	public void mousePressed() {
-		mouseX_temp.add(mouseX);
-		mouseY_temp.add(mouseY);
 
 		redraw();
 	}
