2e312239e1b25dc5acd1cdffd703ec1173a41a36
ito <ito.s.bd@m.titech.ac.jp>
2015-11-03 01:35:35 +0900

    各種メソッド実装

--- src/Models/Usecase.java
+++ src/Models/Usecase.java
@@ -19,23 +19,59 @@ public class Usecase {
 	 */
 	public List<Step> flow;
 
+	public Usecase(String name) {
+		this.name = name;
+		flow = new ArrayList<>();
+	}
+
 	//FlowからMainFlowを抽出
 	public List<Step> getMainFlow() {
-		//TODO
-		return null;
+		List<Step> mainFlow = new ArrayList<>();
+		for (int i = 0; i < flow.size(); i++) {
+			if (flow.get(i).stepType != Step.StepType.ALT_INDEX && flow.get(i).stepType != Step.StepType.EXC_INDEX) {
+				mainFlow.add(flow.get(i));
+			} else {
+				return mainFlow;
+			}
+		}
+		return mainFlow;
 	}
 
 	//FlowからExceptionalFlowのリストを抽出
 	public List<List<Step>> getExceptionalFlowList() {
 		List<List<Step>> exceptionalFlowList = new ArrayList<>();
-		//TODO
+		for (int i = 0; i < flow.size(); i++) {
+			if (flow.get(i).stepType == Step.StepType.EXC_INDEX) {
+				List<Step> exceptionalFlow = new ArrayList<>();
+				for (int j = 0; j < flow.size(); j++) {
+					exceptionalFlow.add(flow.get(i + j));
+					if (flow.get(i + j + 1).stepType == Step.StepType.EXC_INDEX ||
+							(flow.get(i + j + 1).stepType == Step.StepType.ALT_INDEX)) {
+						break;
+					}
+				}
+				exceptionalFlowList.add(exceptionalFlow);
+			}
+		}
 		return exceptionalFlowList;
 	}
 
 	// /FlowからAlternativeFlowのリストを抽出
 	public List<List<Step>> getAlternativeFlowList() {
 		List<List<Step>> alternativeFlowList = new ArrayList<>();
-		//TODO
+		for (int i = 0; i < flow.size(); i++) {
+			if (flow.get(i).stepType == Step.StepType.ALT_INDEX) {
+				List<Step> alternativeFlow = new ArrayList<>();
+				for (int j = 0; j < flow.size(); j++) {
+					alternativeFlow.add(flow.get(i + j));
+					if (flow.get(i + j + 1).stepType == Step.StepType.EXC_INDEX ||
+							(flow.get(i + j + 1).stepType == Step.StepType.ALT_INDEX)) {
+						break;
+					}
+				}
+				alternativeFlowList.add(alternativeFlow);
+			}
+		}
 		return alternativeFlowList;
 	}
 
