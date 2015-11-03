e9de265443504210a840b4a26298330853883038
ito <ito.s.bd@m.titech.ac.jp>
2015-11-04 00:09:50 +0900

    編集ボタン押下処理実装

--- src/Swing/UCEditPanel.java
+++ src/Swing/UCEditPanel.java
@@ -62,7 +62,10 @@ public class UCEditPanel extends JPanel {
 	}
 
 	private void editButtonPressed() {
-
+		Usecase uc = ste.fgm.getUsecaseById(ucg.selectedUsecaseId);
+		uc.name = nameArea.getText();
+		ste.fgm.editUsecase(ucg.selectedUsecaseId, uc);
+		ste.redraw();
 	}
 
 	private void jumpButtonPressed() {
