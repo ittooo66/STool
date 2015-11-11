cd71f191fade79359be9c49e713101695ee09632
ito <ito.s.bd@m.titech.ac.jp>
2015-11-11 17:59:17 +0900

    メトリクス値計測クラス追加

---
+++ src/Metrics/Metrics.java
@@ -0,0 +1,26 @@
+package Metrics;
+
+import Models.FGModel;
+import Models.Usecase;
+
+/**
+ * MetricsŒv‘ªƒNƒ‰ƒX
+ */
+public class Metrics {
+	public static int getANOS(Usecase uc, FGModel fgm) {
+		int ANOS = -1;
+
+		//TODO:
+
+		return ANOS;
+	}
+
+	public static int getACC(Usecase uc, FGModel fgm) {
+		int ACC = -1;
+
+		//TODO:
+
+		return ACC;
+	}
+
+}
