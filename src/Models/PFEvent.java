88049c398af13ced7e3b64d00f750b61795a72cc
ito <ito.s.bd@m.titech.ac.jp>
2015-11-17 01:51:17 +0900

    複数Usecaseからなる構造に変更

--- src/Models/PFEvent.java
+++ src/Models/PFEvent.java
@@ -1,5 +1,8 @@
 package Models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by 66 on 2015/11/17.
  * ProblemFrame用のEventモデル
@@ -11,11 +14,15 @@ public class PFEvent {
 	 * 所属しているInterfaceに対して順方向(true)か、逆方向(false)か
 	 */
 	public boolean inverse;
-	public int rootUsecaseId;
+	public List<Integer> rootUsecaseId;
 
-	public PFEvent(String event, boolean inverse, int rootUsecaseId) {
+	public PFEvent(String event, boolean inverse) {
 		this.event = event;
 		this.inverse = inverse;
-		this.rootUsecaseId = rootUsecaseId;
+		rootUsecaseId = new ArrayList<>();
+	}
+
+	public void setRootUsecase(int usecaseId) {
+		rootUsecaseId.add(usecaseId);
 	}
 }
