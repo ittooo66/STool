a319a7a7c9c7d97398f7bbb08ed3bdd78ddbcfa8
ito <ito.s.bd@m.titech.ac.jp>
2015-11-17 00:56:48 +0900

    InterfaceつくってAdapterそれっぽく使おうとおもって

---
+++ src/Models/FGModel.java
@@ -0,0 +1,28 @@
+package Models;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by 66 on 2015/11/17.
+ * //TODO:つくりかけなのでちゃんとする
+ */
+public interface FGModel {
+	void loadXML(File file);
+
+	void saveXML(File file);
+
+	Goal getGoalById(int id);
+
+	List<Goal> getGoals();
+
+	Domain getDomainById(int id);
+
+	List<Domain> getDomains();
+
+	Usecase getUsecaseById(int id);
+
+	List<Usecase> getUsecases();
+
+
+}
