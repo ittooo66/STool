42aedd24717c70308ec1c7f5bcb6bde84c3d8a36
ito <ito.s.bd@m.titech.ac.jp>
2015-11-18 16:10:52 +0900

    コンテンツ強化

--- src/Processing/Component/ListBoxContent.java
+++ src/Processing/Component/ListBoxContent.java
@@ -7,8 +7,30 @@ public class ListBoxContent {
 	public int id;
 	public String name;
 
+	//塗りつぶすかどうか
+	public boolean isBold;
+
+	//こいつのパラメータ
+	public int param;
+
 	public ListBoxContent(int id, String name) {
 		this.id = id;
 		this.name = name;
+		this.isBold = false;
+		this.param = -1;
+	}
+
+	public ListBoxContent(int id, String name, boolean isBold) {
+		this.id = id;
+		this.name = name;
+		this.isBold = isBold;
+		this.param = -1;
+	}
+
+	public ListBoxContent(int id, String name, int param) {
+		this.id = id;
+		this.name = name;
+		this.isBold = false;
+		this.param = param;
 	}
 }
