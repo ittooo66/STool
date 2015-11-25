ba297a293519a01474562bfdd19f61010b704c4c
ito <ito.s.bd@m.titech.ac.jp>
2015-11-25 16:28:44 +0900

    表示まわりの強化

--- src/Processing/PMetricsBrowse.java
+++ src/Processing/PMetricsBrowse.java
@@ -8,8 +8,11 @@ import Processing.Component.ButtonSetFrame;
 import Processing.Component.COLOR;
 import Processing.Component.ListBox;
 import Processing.Component.ListBoxContent;
+import com.sun.deploy.util.ParameterUtil;
+import com.sun.deploy.util.StringUtils;
 import processing.core.PApplet;
 import processing.core.PFont;
+import processing.event.MouseEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,15 +23,47 @@ public class PMetricsBrowse extends PApplet {
 	private MetricsType metricsType;
 
 	public enum MetricsType {
-		ACC {
+		ACC_ASIS {
 			@Override
 			public MetricsType prev() {
-				return NE;
+				return NE_DIFF;
 			}
-		}, ANOS, UCP, NE {
+
+			public String toString() {
+				return "ACC(As-Is)";
+			}
+		}, ACC_TOBE {
+			public String toString() {
+				return "ACC(To-Be)";
+			}
+		}, ANOS_ASIS {
+			public String toString() {
+				return "ANOS(As-Is)";
+			}
+		}, ANOS_TOBE {
+			public String toString() {
+				return "ANOS(To-Be)";
+			}
+		}, UCP {
+			public String toString() {
+				return "Usecase Point";
+			}
+		}, NE_ASIS {
+			public String toString() {
+				return "NE(As-Is)";
+			}
+		}, NE_TOBE {
+			public String toString() {
+				return "NE(To-Be)";
+			}
+		}, NE_DIFF {
 			@Override
 			public MetricsType next() {
-				return ACC;
+				return ACC_ASIS;
+			}
+
+			public String toString() {
+				return "NE Diff (AsIs-ToBe)";
 			}
 		};
 
@@ -42,33 +77,94 @@ public class PMetricsBrowse extends PApplet {
 
 
 		public static List<ListBoxContent> getList(MetricsType mt, FGModelAdapter fgm) {
+			//詰め込み対象とIDを用意
 			List<ListBoxContent> lbc = new ArrayList<>();
 			int id = 0;
+
+			//現在のバージョンを退避
+			FGModelAdapter.VERSION ver = fgm.getVersion();
+
 			switch (mt) {
-				case ACC:
+				case ACC_ASIS:
+					//バージョン設定
+					fgm.setVersion(FGModelAdapter.VERSION.ASIS);
+					for (Usecase uc : fgm.getUsecases()) {
+						//ACC値取得
+						int param = Metrics.getACC(uc, fgm);
+						//Enable値取得
+						boolean isEnable = fgm.getGoalById(uc.parentLeafGoalId).isEnableForAsIs;
+						//追加
+						lbc.add(new ListBoxContent(id++, uc.name, param, isEnable));
+					}
+					break;
+				case ACC_TOBE:
+					fgm.setVersion(FGModelAdapter.VERSION.TOBE);
 					for (Usecase uc : fgm.getUsecases()) {
 						int param = Metrics.getACC(uc, fgm);
-						boolean isEnable = fgm.getVersion() == FGModelAdapter.VERSION.ASIS ? fgm.getGoalById(uc.parentLeafGoalId).isEnableForAsIs : fgm.getGoalById(uc.parentLeafGoalId).isEnableForToBe;
+						boolean isEnable = fgm.getGoalById(uc.parentLeafGoalId).isEnableForToBe;
 						lbc.add(new ListBoxContent(id++, uc.name, param, isEnable));
 					}
 					break;
-				case ANOS:
+				case ANOS_ASIS:
+					fgm.setVersion(FGModelAdapter.VERSION.ASIS);
 					for (Usecase uc : fgm.getUsecases()) {
 						int param = Metrics.getANOS(uc, fgm);
-						boolean isEnable = fgm.getVersion() == FGModelAdapter.VERSION.ASIS ? fgm.getGoalById(uc.parentLeafGoalId).isEnableForAsIs : fgm.getGoalById(uc.parentLeafGoalId).isEnableForToBe;
+						boolean isEnable = fgm.getGoalById(uc.parentLeafGoalId).isEnableForAsIs;
 						lbc.add(new ListBoxContent(id++, uc.name, param, isEnable));
 					}
 					break;
-				case NE:
+				case ANOS_TOBE:
+					fgm.setVersion(FGModelAdapter.VERSION.TOBE);
+					for (Usecase uc : fgm.getUsecases()) {
+						int param = Metrics.getANOS(uc, fgm);
+						boolean isEnable = fgm.getGoalById(uc.parentLeafGoalId).isEnableForToBe;
+						lbc.add(new ListBoxContent(id++, uc.name, param, isEnable));
+					}
+					break;
+				case NE_ASIS:
+					fgm.setVersion(FGModelAdapter.VERSION.ASIS);
 					for (Domain d : fgm.getDomains()) {
+						String name = Domain.DomainType.getPrefix(d.domainType) + ":" + d.name;
 						int param = Metrics.getNE(d, fgm);
-						lbc.add(new ListBoxContent(id++, Domain.DomainType.getPrefix(d.domainType) + ":" + d.name, param));
+						boolean isBold = d.domainType == Domain.DomainType.BIDDABLE;
+						lbc.add(new ListBoxContent(id++, name, param, isBold));
+					}
+					break;
+				case NE_TOBE:
+					fgm.setVersion(FGModelAdapter.VERSION.TOBE);
+					for (Domain d : fgm.getDomains()) {
+						String name = Domain.DomainType.getPrefix(d.domainType) + ":" + d.name;
+						int param = Metrics.getNE(d, fgm);
+						boolean isBold = d.domainType == Domain.DomainType.BIDDABLE;
+						lbc.add(new ListBoxContent(id++, name, param, isBold));
+					}
+					break;
+				case NE_DIFF:
+					for (Domain d : fgm.getDomains()) {
+						fgm.setVersion(FGModelAdapter.VERSION.ASIS);
+						int paramAsis = Metrics.getNE(d, fgm);
+						fgm.setVersion(FGModelAdapter.VERSION.TOBE);
+						int paramTobe = Metrics.getNE(d, fgm);
+						String name = Domain.DomainType.getPrefix(d.domainType) + ":" + d.name;
+						boolean isBold = d.domainType == Domain.DomainType.BIDDABLE;
+						lbc.add(new ListBoxContent(id++, name, paramAsis - paramTobe, isBold));
 					}
 					break;
 				case UCP:
-					int param = Metrics.getUCP(fgm);
-					lbc.add(new ListBoxContent(id++, "Usecase Point", param));
+					fgm.setVersion(FGModelAdapter.VERSION.ASIS);
+					int paramAsis = Metrics.getUCP(fgm);
+					lbc.add(new ListBoxContent(id++, "Usecase Point(As-Is)", paramAsis));
+					fgm.setVersion(FGModelAdapter.VERSION.TOBE);
+					int paramTobe = Metrics.getUCP(fgm);
+					lbc.add(new ListBoxContent(id++, "Usecase Point(To-Be)", paramTobe));
+					int paramDiff = paramAsis - paramTobe;
+					lbc.add(new ListBoxContent(id++, "Usecase Point Diff(AsIs - ToBe)", paramDiff));
+					break;
 			}
+
+			//バージョン戻し
+			fgm.setVersion(ver);
+
 			return lbc;
 		}
 	}
@@ -95,7 +191,7 @@ public class PMetricsBrowse extends PApplet {
 		bsf.addButton("→");
 		lb = new ListBox();
 
-		metricsType = MetricsType.ANOS;
+		metricsType = MetricsType.ANOS_ASIS;
 	}
 
 	//変更フラグ
@@ -113,7 +209,7 @@ public class PMetricsBrowse extends PApplet {
 		background(255);
 
 		//Buttonsetframe更新して記述
-		bsf.setTitle(metricsType.toString() + "(" + FGModelAdapter.VERSION.getString(fgm.getVersion()) + ")");
+		bsf.setTitle(metricsType.toString());
 		bsf.adjust(20, 20, width - 60, 30);
 		bsf.draw(this);
 		//ListBox詰め込んで記述
@@ -137,6 +233,12 @@ public class PMetricsBrowse extends PApplet {
 				metricsType = metricsType.next();
 				break;
 		}
+		redraw();
+	}
+
+	public void mouseWheel(MouseEvent event) {
+		int e = event.getCount() > 0 ? 1 : -1;
+		if (lb.isOn(mouseX, mouseY)) lb.scroll(e);
 		redraw();
 	}
 
