08900bd02597ec7e626eeb64b65b31d3fd407982
ito <ito.s.bd@m.titech.ac.jp>
2015-12-02 11:04:44 +0900

    シナリオモデル追加

---
+++ src/Models/Scenario.java
@@ -0,0 +1,13 @@
+package Models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scenario {
+	//Step列をシナリオとみなす
+	public List<Step> steps;
+
+	public Scenario() {
+		steps = new ArrayList<>();
+	}
+}
